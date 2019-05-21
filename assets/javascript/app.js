a58f87444c023edf095b42b268e3e9923b29753e
Fabulous_Rachael <rachael.whitefield@gmail.com>
2019-05-20 22:09:58 -0600

    giphy add button working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,13 +5,15 @@ function renderButtons() {
   $("#buttons-view").empty();
   for (var i=0; i < topics.length; i++) {
     $("#buttons-view").append("<button class='btn btn-success' data-giphy='" + topics[i] + "'>" + topics[i] + "<button>");
-  //   var a = $("<button>");
-  //   a.addClass("giphy");
-  //   a.attr("giphy-name", topics[i]);
-  //   a.text(topics[i]);
-  //   $("#buttons-view").append(a);
-  // }
-}
-}
+    }
+  }
 renderButtons();
+
+$("#add-giphy").on("click", function() {
+  event.preventDefault();
+  var giphy = $("#giphy-input").val().trim();
+  topics.push(giphy);
+  renderButtons();
+  return;
+})
 });

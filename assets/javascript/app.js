5582e08b19adbb7403f91242fd4e536d739e2cf9
Fabulous_Rachael <rachael.whitefield@gmail.com>
2019-05-21 16:05:28 -0600

    new button created and added new giphy working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,32 +4,49 @@ var topics = ["Family Guy", "The Simpsons", "Bob's Burgers", "Archer", "Spongebo
 function renderButtons() {
   $("#buttons-view").empty();
   for (var i=0; i < topics.length; i++) {
-    $("#buttons-view").append("<button class='btn btn-success data-giphy'" + topics[i] + ">" + topics[i] + "<button>");
-    }
+    var button = $("<button>");
+    button.addClass("topic");
+    button.attr("data-topic", topics[i]);
+    button.text(topics[i]);
+    $("#buttons-view").append(button);
   }
+}
 renderButtons();
 
-$("#add-giphy").on("click", function() {
-  event.preventDefault();
-  var giphy = $("#giphy-input").val().trim();
-  topics.push(giphy);
-  renderButtons();
-  return;
-})
+// $("#add-giphy").on("click", function() {
+//   event.preventDefault();
+//   var giphy = $("#giphy-input").val().trim();
+//   topics.push(giphy);
+//   renderButtons();
+//   return;
+// })
 
-$(".data-giphy").on("click", function() {
+$("#add-giphy").on("click", function(event){
+  event.preventDefault();
+  var topic = $("#giphy-input").val().trim();
+  topics.push(topic);
+  renderButtons();
+})
+$(document).on("click", ".topic", displayTopicInfo);
+
+renderButtons();
+
+// $(".topic").on("click", function() {
+  function displayTopicInfo(){
   var topic = $(this).attr("data-topic");
   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
   topic + "&api_key=TSUNPTa6z2tklGuh4PZEaPMuEogApupk&=10";
   
-
+  // $("#theme-view").empty();
   $.ajax({
     url: queryURL,
     method: "GET"
   })
   .then(function(response){
     var results = response.data;
+    console.log(response);
 
+    
     for(var i=0; i<10; i++) {
       var gifDiv = $("<div>");
 
@@ -38,7 +55,7 @@ $(".data-giphy").on("click", function() {
       var p = $("<p>").text("Rating: " + rating);
 
       var topicImage = $("<img>");
-      topicImage.attr("src", results[i].images.fixed_height.url);
+      topicImage.attr("src", results[i].images.fixed_height_still.url);
 
       gifDiv.prepend(p);
       gifDiv.prepend(topicImage);
@@ -46,5 +63,5 @@ $(".data-giphy").on("click", function() {
       $("#giphy-view").prepend(gifDiv);
     }
   })
-})
+}
 });
